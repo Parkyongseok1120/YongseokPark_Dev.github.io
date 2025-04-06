0b892612a6cb33bf16dcf42cdd0213c2c99ae936
Yongseok Park <ssters999@outlook.kr>
2025-04-06 19:24:09 +0900

    1

--- components/Footer.js
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-    return (
-      <footer className="p-4 bg-gray-800 text-white text-center">
-        <p>&copy; {new Date().getFullYear()} YongseokPark Dev-log. All rights reserved.</p>
-      </footer>
-    );
-  }
-  
